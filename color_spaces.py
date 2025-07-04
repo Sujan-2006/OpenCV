f3c30dcee78ef4ba93f8cb8559008a71b878182e
Sujan-2006 <sujan032006@gmail.com>
2025-07-04 09:43:54 +0530

    splitting and merging of image color channels

--- color_spaces.py
+++ color_spaces.py
@@ -1,8 +1,11 @@
 import cv2 as cv
+import numpy as np
 
 img = cv.imread("./photos/dog.jpg")
 
-cv.imshow("Original dog",img)
+blank = np.zeros(img.shape[:2], dtype='uint8')
+
+cv.imshow("Original dog", img)
 
 gray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 cv.imshow("Gray", gray)
@@ -16,6 +19,31 @@ lab_bgr= cv.cvtColor(lab, cv.COLOR_LAB2BGR)
 cv.imshow("LAB to BGR", lab_bgr)
 hsv_bgr = cv.cvtColor(hsv, cv.COLOR_HSV2BGR)
 cv.imshow("HSV to BGR", hsv_bgr)
+
+#to split the image
+b,g,r=cv.split(img)
+
+blue=cv.merge([b,blank,blank])
+green=cv.merge([blank,g,blank])
+red=cv.merge([blank,blank,r])
+cv.imshow("blue",blue)
+cv.imshow("green",green)
+cv.imshow("red",red)
+
+
+cv.imshow("BLUE",b)
+cv.imshow("GREEN",g)
+cv.imshow("RED",r)
+
+#to print the shape of images
+print(img.shape)
+print(b.shape)
+print(g.shape)
+print(r.shape)
+
+#to merge the image
+merged=cv.merge([b,g,r])
+cv.imshow("Merged Image",merged)
 
 cv.waitKey(0)
 
