5e9bd2d9f7388862c2fac410b62e6c20eaefc868
Sujan-2006 <sujan032006@gmail.com>
2025-07-04 15:15:03 +0530

    Colored histogram for a masked image

--- histogram.py
+++ histogram.py
@@ -7,24 +7,26 @@ cv.imshow("LZS",img)
 
 blank=np.zeros(img.shape[:2],dtype='uint8')
 
+'''
 gray=cv.cvtColor(img,cv.COLOR_BGR2GRAY)
 cv.imshow("Gray",gray)
+'''
+mask=cv.circle(blank.copy(),(img.shape[1]//2,img.shape[0]//2),200,255,-1)
 
-circle=cv.circle(blank.copy(),(img.shape[1]//2,img.shape[0]//2),200,255,-1)
+masked=cv.bitwise_and(img,img,mask=mask)
+cv.imshow("Mask",masked)
 
-mask=cv.bitwise_and(gray,gray,mask=circle)
-cv.imshow("Mask",mask)
-
-#HISTOGRAM
-hist=cv.calcHist([gray],[0],mask,[256],[0,256])
+#COLORED HISTOGRAM
 
 plt.figure()
 plt.title("HISTOGRAM")
 plt.xlabel("Bins")
 plt.ylabel("no of pixels")
-plt.plot(hist)
-plt.xlim([0,256])
+colors=('b','g','r')
+for i,clo in enumerate(colors):
+    hist=cv.calcHist([img],[i],mask,[256],[0,256])    #change mask to None to get the colored histogram of the whole image
+    plt.plot(hist,color=clo)
+    plt.xlim([0,256])
 plt.show()
- 
 
 cv.waitKey(0)
