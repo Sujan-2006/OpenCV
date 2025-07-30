a616c1b4a8647ffef3754550ce68be1de66447b0
Sujan-2006 <sujan032006@gmail.com>
2025-07-30 17:44:55 +0530

    Performing ROI(Region Of Interest)

--- operations.py
+++ operations.py
@@ -1,14 +1,7 @@
 import cv2 as cv
 import numpy as np
-img=cv.imread("./photos/dog.jpg")
-print(img.shape)
-print(img.size)
-print(img.dtype)
-
-b,g,r=cv.split(img)
-cv.imshow("Blue",b)
-cv.imshow("Green",g)           
-img=cv.merge((b,g,r))
-cv.imshow("Dog",img)
-
+img = cv.imread("./photos/LZS.jpg")
+flower = img[350:410,400:460]  
+img[343:403,170:230] = flower   
+cv.imshow("Dog", img)
 cv.waitKey(0)
