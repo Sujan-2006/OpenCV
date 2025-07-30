4a9b67499674060c5d0c6389a5a973705a3c498b
Sujan-2006 <sujan032006@gmail.com>
2025-07-30 17:50:46 +0530

    adding two images of two images

--- operations.py
+++ operations.py
@@ -1,7 +1,10 @@
 import cv2 as cv
 import numpy as np
-img = cv.imread("./photos/LZS.jpg")
-flower = img[350:410,400:460]  
-img[343:403,170:230] = flower   
-cv.imshow("Dog", img)
+img1= cv.imread("./photos/LZS.jpg")
+img2= cv.imread("./photos/dog.jpg")
+img1= cv.resize(img1, (300, 300))
+img2 = cv.resize(img2, (300, 300))
+img3=cv.add(img1,img2)
+  
+cv.imshow("Combined", img3)
 cv.waitKey(0)
