a900b703b62bca041c31799a564e2f4af7a8fc92
Sujan-2006 <sujan032006@gmail.com>
2025-08-02 14:43:26 +0530

    tracking colors in videos

--- obj_det.py
+++ obj_det.py
@@ -4,6 +4,8 @@ import numpy as np
 def nothing(x):
     print(x)
 
+capture=cv.VideoCapture(0)
+
 cv.namedWindow("TRACKING")
 cv.createTrackbar("LowHue", "TRACKING", 0, 255, nothing)
 cv.createTrackbar("LowSat", "TRACKING", 0, 255, nothing)
@@ -13,8 +15,10 @@ cv.createTrackbar("UpSat", "TRACKING", 255, 255, nothing)
 cv.createTrackbar("UpVal", "TRACKING", 255, 255, nothing)
 
 while(1):
-    img=cv.imread("./photos/balls.jpg") 
-    hsv=cv.cvtColor(img,cv.COLOR_BGR2HSV)
+    #img=cv.imread("./photos/balls.jpg") 
+    _,frame=capture.read()
+
+    hsv=cv.cvtColor(frame,cv.COLOR_BGR2HSV)
 
     LH=cv.getTrackbarPos("LowHue","TRACKING")
     LS=cv.getTrackbarPos("LowSat","TRACKING")
@@ -27,13 +31,14 @@ while(1):
     ub=np.array([UH,US,UV])           #blue  130,255,255
 
     mask=cv.inRange(hsv,lb,ub)
-    output=cv.bitwise_and(img,img,mask=mask)
+    output=cv.bitwise_and(frame,frame,mask=mask)
 
-    cv.imshow("Tracking",img)
+    cv.imshow("Tracking",frame)
     cv.imshow("Mask",mask)
     cv.imshow("RESULT",output)
 
     if cv.waitKey(1) & 0xFF==ord("a"):
         break
+capture.release()
 cv.destroyAllWindows()
   
